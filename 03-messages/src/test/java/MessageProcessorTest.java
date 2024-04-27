4ec30c20f60dcca561903946ba6309c92ffba94c
Marion Andermatt <marion.andermatt@gmx.ch>
2024-04-27 20:41:14 +0200

    B. Content of invocations - ArgumentCaptor

--- 03-messages/src/test/java/MessageProcessorTest.java
+++ 03-messages/src/test/java/MessageProcessorTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -9,6 +10,8 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class MessageProcessorTest {
@@ -77,6 +80,93 @@ class MessageProcessorTest {
 
         verify(messageService, times(2)).sendMessage(anyString(), anyString());
         verify(messageService, times(2)).sendMessage(message.getReceiver(), message.getContent());
+    }
+
+    @Test
+    void rightContent() {
+        MessageProcessor messageProcessor = new MessageProcessor(messageService);
+
+        Message message = new Message("sender", "receiver", "content");
+
+        List<Message> messages = Arrays.asList(message);
+
+        doNothing().when(messageService).sendMessage(anyString(), anyString());
+
+        messageProcessor.processMessages(messages);
+
+        ArgumentCaptor<String> receiverCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+
+
+        verify(messageService).sendMessage(receiverCaptor.capture(), contentCaptor.capture());
+
+        String receiver = receiverCaptor.getValue();
+        String content = contentCaptor.getValue();
+
+        assertEquals("receiver", receiver);
+        assertEquals("content", content);
+    }
+
+    @Test
+    void nullContent() {
+        MessageProcessor messageProcessor = new MessageProcessor(messageService);
+
+        Message message = new Message("sender", null, null);
+
+        List<Message> messages = Arrays.asList(message);
+
+        doNothing().when(messageService).sendMessage(null, null);
+
+        messageProcessor.processMessages(messages);
+
+        ArgumentCaptor<String> receiverCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+
+
+        verify(messageService).sendMessage(receiverCaptor.capture(), contentCaptor.capture());
+
+        String receiver = receiverCaptor.getValue();
+        String content = contentCaptor.getValue();
+
+        assertEquals(null, receiver);
+        assertEquals(null, content);
+    }
+
+    @Test
+    void rightContentMultipleInvocation() {
+        MessageProcessor messageProcessor = new MessageProcessor(messageService);
+
+        Message message = new Message("sender", "Ronald", "software");
+        Message message2 = new Message("sender2", "Robert", "Testing");
+        Message message3 = new Message("sender3", "Ruedi", "Module FS24");
+
+
+        List<Message> messages = Arrays.asList(message, message2, message3, message3, message2);
+
+        doNothing().when(messageService).sendMessage(anyString(), anyString());
+
+        messageProcessor.processMessages(messages);
+
+        ArgumentCaptor<String> receiverCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+
+
+        verify(messageService, times(5)).sendMessage(receiverCaptor.capture(), contentCaptor.capture());
+
+        List<String> receivers = receiverCaptor.getAllValues();
+        List<String> contents = contentCaptor.getAllValues();
+
+        assertEquals("Ronald", receivers.get(0));
+        assertEquals("Robert", receivers.get(1));
+        assertEquals("Ruedi", receivers.get(2));
+        assertEquals("Ruedi", receivers.get(3));
+        assertEquals("Robert", receivers.get(4));
+
+        assertEquals("software", contents.get(0));
+        assertEquals("Testing", contents.get(1));
+        assertEquals("Module FS24", contents.get(2));
+        assertEquals("Module FS24", contents.get(3));
+        assertEquals("Testing", contents.get(4));
     }
 
 }
